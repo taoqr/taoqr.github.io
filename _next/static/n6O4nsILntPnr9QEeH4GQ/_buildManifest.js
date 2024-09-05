9364800c1f3de121803fb5590f1280d1ba3f067e
huytechvn <huy.np@outlook.com>
2024-09-05 15:02:39 +0700

    first commit

---
+++ _next/static/n6O4nsILntPnr9QEeH4GQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/962-7b0522fead77b01c.js","static/chunks/pages/index-f39df71e58b0aa60.js"],"/_error":["static/chunks/pages/_error-1be831200e60c5c0.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
